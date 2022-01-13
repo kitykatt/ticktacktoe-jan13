6024c402581e5f3b7b43ce8cbe0d3c9996b169b1
kitykatt <90800597+kitykatt@users.noreply.github.com>
2022-01-13 14:04:35 -0700

    Add files via upload

---
+++ tictaktoe/tictaktoe/Form1.Designer.vb
@@ -0,0 +1,213 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.B1 = New System.Windows.Forms.Button()
+        Me.B2 = New System.Windows.Forms.Button()
+        Me.B3 = New System.Windows.Forms.Button()
+        Me.B4 = New System.Windows.Forms.Button()
+        Me.B5 = New System.Windows.Forms.Button()
+        Me.B6 = New System.Windows.Forms.Button()
+        Me.B7 = New System.Windows.Forms.Button()
+        Me.B8 = New System.Windows.Forms.Button()
+        Me.B9 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.BackgroundWorker1 = New System.ComponentModel.BackgroundWorker()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'B1
+        '
+        Me.B1.BackColor = System.Drawing.SystemColors.Control
+        Me.B1.Font = New System.Drawing.Font("Microsoft Sans Serif", 48.0!)
+        Me.B1.Location = New System.Drawing.Point(1, 19)
+        Me.B1.Margin = New System.Windows.Forms.Padding(0)
+        Me.B1.Name = "B1"
+        Me.B1.Padding = New System.Windows.Forms.Padding(3)
+        Me.B1.Size = New System.Drawing.Size(75, 75)
+        Me.B1.TabIndex = 0
+        Me.B1.UseVisualStyleBackColor = False
+        '
+        'B2
+        '
+        Me.B2.BackColor = System.Drawing.SystemColors.Control
+        Me.B2.Font = New System.Drawing.Font("Microsoft Sans Serif", 48.0!)
+        Me.B2.Location = New System.Drawing.Point(82, 19)
+        Me.B2.Margin = New System.Windows.Forms.Padding(0)
+        Me.B2.Name = "B2"
+        Me.B2.Padding = New System.Windows.Forms.Padding(3)
+        Me.B2.Size = New System.Drawing.Size(75, 75)
+        Me.B2.TabIndex = 1
+        Me.B2.UseVisualStyleBackColor = False
+        '
+        'B3
+        '
+        Me.B3.BackColor = System.Drawing.SystemColors.Control
+        Me.B3.Font = New System.Drawing.Font("Microsoft Sans Serif", 48.0!)
+        Me.B3.Location = New System.Drawing.Point(163, 19)
+        Me.B3.Margin = New System.Windows.Forms.Padding(0)
+        Me.B3.Name = "B3"
+        Me.B3.Padding = New System.Windows.Forms.Padding(3)
+        Me.B3.Size = New System.Drawing.Size(75, 75)
+        Me.B3.TabIndex = 2
+        Me.B3.UseVisualStyleBackColor = False
+        '
+        'B4
+        '
+        Me.B4.BackColor = System.Drawing.SystemColors.Control
+        Me.B4.Font = New System.Drawing.Font("Microsoft Sans Serif", 48.0!)
+        Me.B4.Location = New System.Drawing.Point(1, 100)
+        Me.B4.Margin = New System.Windows.Forms.Padding(0)
+        Me.B4.Name = "B4"
+        Me.B4.Padding = New System.Windows.Forms.Padding(3)
+        Me.B4.Size = New System.Drawing.Size(75, 75)
+        Me.B4.TabIndex = 5
+        Me.B4.UseVisualStyleBackColor = False
+        '
+        'B5
+        '
+        Me.B5.BackColor = System.Drawing.SystemColors.Control
+        Me.B5.Font = New System.Drawing.Font("Microsoft Sans Serif", 48.0!)
+        Me.B5.Location = New System.Drawing.Point(82, 100)
+        Me.B5.Margin = New System.Windows.Forms.Padding(0)
+        Me.B5.Name = "B5"
+        Me.B5.Padding = New System.Windows.Forms.Padding(3)
+        Me.B5.Size = New System.Drawing.Size(75, 75)
+        Me.B5.TabIndex = 4
+        Me.B5.UseVisualStyleBackColor = False
+        '
+        'B6
+        '
+        Me.B6.BackColor = System.Drawing.SystemColors.Control
+        Me.B6.Font = New System.Drawing.Font("Microsoft Sans Serif", 48.0!)
+        Me.B6.Location = New System.Drawing.Point(163, 100)
+        Me.B6.Margin = New System.Windows.Forms.Padding(0)
+        Me.B6.Name = "B6"
+        Me.B6.Padding = New System.Windows.Forms.Padding(3)
+        Me.B6.Size = New System.Drawing.Size(75, 75)
+        Me.B6.TabIndex = 3
+        Me.B6.UseVisualStyleBackColor = False
+        '
+        'B7
+        '
+        Me.B7.BackColor = System.Drawing.SystemColors.Control
+        Me.B7.Font = New System.Drawing.Font("Microsoft Sans Serif", 48.0!)
+        Me.B7.Location = New System.Drawing.Point(1, 181)
+        Me.B7.Margin = New System.Windows.Forms.Padding(0)
+        Me.B7.Name = "B7"
+        Me.B7.Padding = New System.Windows.Forms.Padding(3)
+        Me.B7.Size = New System.Drawing.Size(75, 75)
+        Me.B7.TabIndex = 8
+        Me.B7.UseVisualStyleBackColor = False
+        '
+        'B8
+        '
+        Me.B8.BackColor = System.Drawing.SystemColors.Control
+        Me.B8.Font = New System.Drawing.Font("Microsoft Sans Serif", 48.0!)
+        Me.B8.Location = New System.Drawing.Point(82, 181)
+        Me.B8.Margin = New System.Windows.Forms.Padding(0)
+        Me.B8.Name = "B8"
+        Me.B8.Padding = New System.Windows.Forms.Padding(3)
+        Me.B8.Size = New System.Drawing.Size(75, 75)
+        Me.B8.TabIndex = 7
+        Me.B8.UseVisualStyleBackColor = False
+        '
+        'B9
+        '
+        Me.B9.BackColor = System.Drawing.SystemColors.Control
+        Me.B9.Font = New System.Drawing.Font("Microsoft Sans Serif", 48.0!)
+        Me.B9.Location = New System.Drawing.Point(163, 181)
+        Me.B9.Margin = New System.Windows.Forms.Padding(0)
+        Me.B9.Name = "B9"
+        Me.B9.Padding = New System.Windows.Forms.Padding(3)
+        Me.B9.Size = New System.Drawing.Size(75, 75)
+        Me.B9.TabIndex = 6
+        Me.B9.UseVisualStyleBackColor = False
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.Button1.Location = New System.Drawing.Point(1, 262)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(237, 34)
+        Me.Button1.TabIndex = 9
+        Me.Button1.Text = "Clear"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(23, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(39, 13)
+        Me.Label1.TabIndex = 10
+        Me.Label1.Text = "Label1"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(176, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(39, 13)
+        Me.Label2.TabIndex = 11
+        Me.Label2.Text = "Label2"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(236, 298)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.B7)
+        Me.Controls.Add(Me.B8)
+        Me.Controls.Add(Me.B9)
+        Me.Controls.Add(Me.B4)
+        Me.Controls.Add(Me.B5)
+        Me.Controls.Add(Me.B6)
+        Me.Controls.Add(Me.B3)
+        Me.Controls.Add(Me.B2)
+        Me.Controls.Add(Me.B1)
+        Me.Name = "Form1"
+        Me.Text = " "
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents B1 As Button
+    Friend WithEvents B2 As Button
+    Friend WithEvents B3 As Button
+    Friend WithEvents B4 As Button
+    Friend WithEvents B5 As Button
+    Friend WithEvents B6 As Button
+    Friend WithEvents B7 As Button
+    Friend WithEvents B8 As Button
+    Friend WithEvents B9 As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+End Class
---
+++ tictaktoe/tictaktoe/Form1.vb
@@ -0,0 +1,74 @@
+﻿Public Class Form1
+    Dim go As Integer = 0
+    Dim xs As Integer = 0
+    Dim os As Integer = 0
+    Dim stp As Integer = 0
+    Private Sub B1_Click(sender As Object, e As EventArgs) Handles B1.Click, B2.Click, B3.Click, B4.Click, B5.Click, B6.Click, B7.Click, B8.Click, B9.Click
+        If Me.BackColor = DefaultBackColor Then
+            If (sender.Text = "" And go = 0 Or sender.Text = "O") Then
+                sender.Text = "X"
+                go = 1
+            ElseIf (sender.Text = "" And go = 1 Or sender.Text = "X") Then
+                sender.Text = "O"
+                go = 0
+            End If
+        End If
+        If winner() Then
+            If stp = 0 Then
+                stp = 1
+                If go = 1 Then
+                    xs = xs + 1
+                    Label1.Text = xs
+                End If
+                If go = 0 Then
+                    os = os + 1
+                    Label2.Text = os
+                End If
+                Me.BackColor = Color.Green
+                B1.BackColor = Color.Green
+                B2.BackColor = Color.Green
+                B3.BackColor = Color.Green
+                B4.BackColor = Color.Green
+                B5.BackColor = Color.Green
+                B6.BackColor = Color.Green
+                B7.BackColor = Color.Green
+                B8.BackColor = Color.Green
+                B9.BackColor = Color.Green
+            End If
+        End If
+    End Sub
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        go = 0
+        stp = 0
+        B1.Text = ""
+        B2.Text = ""
+        B3.Text = ""
+        B4.Text = ""
+        B5.Text = ""
+        B6.Text = ""
+        B7.Text = ""
+        B8.Text = ""
+        B9.Text = ""
+        Me.BackColor = DefaultBackColor
+        B1.BackColor = DefaultBackColor
+        B2.BackColor = DefaultBackColor
+        B3.BackColor = DefaultBackColor
+        B4.BackColor = DefaultBackColor
+        B5.BackColor = DefaultBackColor
+        B6.BackColor = DefaultBackColor
+        B7.BackColor = DefaultBackColor
+        B8.BackColor = DefaultBackColor
+        B9.BackColor = DefaultBackColor
+    End Sub
+    Function winner() As Boolean
+        If B1.Text = B2.Text And B2.Text = B3.Text And B1.Text <> "" Then Return True
+        If B4.Text = B5.Text And B5.Text = B6.Text And B4.Text <> "" Then Return True
+        If B7.Text = B8.Text And B8.Text = B9.Text And B7.Text <> "" Then Return True
+        If B1.Text = B4.Text And B4.Text = B7.Text And B1.Text <> "" Then Return True
+        If B2.Text = B5.Text And B5.Text = B8.Text And B2.Text <> "" Then Return True
+        If B3.Text = B6.Text And B6.Text = B9.Text And B3.Text <> "" Then Return True
+        If B1.Text = B5.Text And B5.Text = B9.Text And B1.Text <> "" Then Return True
+        If B3.Text = B5.Text And B5.Text = B7.Text And B3.Text <> "" Then Return True
+        Return False
+    End Function
+End Class
