df9af23089dfbef4dfccbf6a14e97f7a4c102ef2
kitykatt <90800597+kitykatt@users.noreply.github.com>
2022-01-14 23:47:07 -0700

    Jan14

--- tictaktoe/tictaktoe/Form1.Designer.vb
+++ tictaktoe/tictaktoe/Form1.Designer.vb
@@ -35,13 +35,25 @@ Partial Class Form1
         Me.BackgroundWorker1 = New System.ComponentModel.BackgroundWorker()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.Label2 = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton3 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton4 = New System.Windows.Forms.RadioButton()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
         Me.SuspendLayout()
         '
         'B1
         '
         Me.B1.BackColor = System.Drawing.SystemColors.Control
         Me.B1.Font = New System.Drawing.Font("Microsoft Sans Serif", 48.0!)
-        Me.B1.Location = New System.Drawing.Point(1, 19)
+        Me.B1.Location = New System.Drawing.Point(3, 19)
         Me.B1.Margin = New System.Windows.Forms.Padding(0)
         Me.B1.Name = "B1"
         Me.B1.Padding = New System.Windows.Forms.Padding(3)
@@ -53,7 +65,7 @@ Partial Class Form1
         '
         Me.B2.BackColor = System.Drawing.SystemColors.Control
         Me.B2.Font = New System.Drawing.Font("Microsoft Sans Serif", 48.0!)
-        Me.B2.Location = New System.Drawing.Point(82, 19)
+        Me.B2.Location = New System.Drawing.Point(84, 19)
         Me.B2.Margin = New System.Windows.Forms.Padding(0)
         Me.B2.Name = "B2"
         Me.B2.Padding = New System.Windows.Forms.Padding(3)
@@ -65,7 +77,7 @@ Partial Class Form1
         '
         Me.B3.BackColor = System.Drawing.SystemColors.Control
         Me.B3.Font = New System.Drawing.Font("Microsoft Sans Serif", 48.0!)
-        Me.B3.Location = New System.Drawing.Point(163, 19)
+        Me.B3.Location = New System.Drawing.Point(165, 19)
         Me.B3.Margin = New System.Windows.Forms.Padding(0)
         Me.B3.Name = "B3"
         Me.B3.Padding = New System.Windows.Forms.Padding(3)
@@ -77,7 +89,7 @@ Partial Class Form1
         '
         Me.B4.BackColor = System.Drawing.SystemColors.Control
         Me.B4.Font = New System.Drawing.Font("Microsoft Sans Serif", 48.0!)
-        Me.B4.Location = New System.Drawing.Point(1, 100)
+        Me.B4.Location = New System.Drawing.Point(3, 100)
         Me.B4.Margin = New System.Windows.Forms.Padding(0)
         Me.B4.Name = "B4"
         Me.B4.Padding = New System.Windows.Forms.Padding(3)
@@ -89,7 +101,7 @@ Partial Class Form1
         '
         Me.B5.BackColor = System.Drawing.SystemColors.Control
         Me.B5.Font = New System.Drawing.Font("Microsoft Sans Serif", 48.0!)
-        Me.B5.Location = New System.Drawing.Point(82, 100)
+        Me.B5.Location = New System.Drawing.Point(84, 100)
         Me.B5.Margin = New System.Windows.Forms.Padding(0)
         Me.B5.Name = "B5"
         Me.B5.Padding = New System.Windows.Forms.Padding(3)
@@ -101,7 +113,7 @@ Partial Class Form1
         '
         Me.B6.BackColor = System.Drawing.SystemColors.Control
         Me.B6.Font = New System.Drawing.Font("Microsoft Sans Serif", 48.0!)
-        Me.B6.Location = New System.Drawing.Point(163, 100)
+        Me.B6.Location = New System.Drawing.Point(165, 100)
         Me.B6.Margin = New System.Windows.Forms.Padding(0)
         Me.B6.Name = "B6"
         Me.B6.Padding = New System.Windows.Forms.Padding(3)
@@ -113,7 +125,7 @@ Partial Class Form1
         '
         Me.B7.BackColor = System.Drawing.SystemColors.Control
         Me.B7.Font = New System.Drawing.Font("Microsoft Sans Serif", 48.0!)
-        Me.B7.Location = New System.Drawing.Point(1, 181)
+        Me.B7.Location = New System.Drawing.Point(3, 181)
         Me.B7.Margin = New System.Windows.Forms.Padding(0)
         Me.B7.Name = "B7"
         Me.B7.Padding = New System.Windows.Forms.Padding(3)
@@ -125,7 +137,7 @@ Partial Class Form1
         '
         Me.B8.BackColor = System.Drawing.SystemColors.Control
         Me.B8.Font = New System.Drawing.Font("Microsoft Sans Serif", 48.0!)
-        Me.B8.Location = New System.Drawing.Point(82, 181)
+        Me.B8.Location = New System.Drawing.Point(84, 181)
         Me.B8.Margin = New System.Windows.Forms.Padding(0)
         Me.B8.Name = "B8"
         Me.B8.Padding = New System.Windows.Forms.Padding(3)
@@ -137,7 +149,7 @@ Partial Class Form1
         '
         Me.B9.BackColor = System.Drawing.SystemColors.Control
         Me.B9.Font = New System.Drawing.Font("Microsoft Sans Serif", 48.0!)
-        Me.B9.Location = New System.Drawing.Point(163, 181)
+        Me.B9.Location = New System.Drawing.Point(165, 181)
         Me.B9.Margin = New System.Windows.Forms.Padding(0)
         Me.B9.Name = "B9"
         Me.B9.Padding = New System.Windows.Forms.Padding(3)
@@ -148,7 +160,7 @@ Partial Class Form1
         'Button1
         '
         Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
-        Me.Button1.Location = New System.Drawing.Point(1, 262)
+        Me.Button1.Location = New System.Drawing.Point(3, 262)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(237, 34)
         Me.Button1.TabIndex = 9
@@ -158,42 +170,152 @@ Partial Class Form1
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(23, 0)
+        Me.Label1.Location = New System.Drawing.Point(39, 0)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(39, 13)
+        Me.Label1.Size = New System.Drawing.Size(13, 13)
         Me.Label1.TabIndex = 10
-        Me.Label1.Text = "Label1"
+        Me.Label1.Text = "0"
         '
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(176, 0)
+        Me.Label2.Location = New System.Drawing.Point(185, 0)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(39, 13)
+        Me.Label2.Size = New System.Drawing.Size(13, 13)
         Me.Label2.TabIndex = 11
-        Me.Label2.Text = "Label2"
+        Me.Label2.Text = "0"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Button3)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.B1)
+        Me.GroupBox1.Controls.Add(Me.B2)
+        Me.GroupBox1.Controls.Add(Me.Button1)
+        Me.GroupBox1.Controls.Add(Me.B3)
+        Me.GroupBox1.Controls.Add(Me.B7)
+        Me.GroupBox1.Controls.Add(Me.B6)
+        Me.GroupBox1.Controls.Add(Me.B8)
+        Me.GroupBox1.Controls.Add(Me.B5)
+        Me.GroupBox1.Controls.Add(Me.B9)
+        Me.GroupBox1.Controls.Add(Me.B4)
+        Me.GroupBox1.Location = New System.Drawing.Point(3, 2)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(244, 301)
+        Me.GroupBox1.TabIndex = 12
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Visible = False
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.Button2)
+        Me.GroupBox2.Controls.Add(Me.RadioButton4)
+        Me.GroupBox2.Controls.Add(Me.RadioButton3)
+        Me.GroupBox2.Controls.Add(Me.RadioButton2)
+        Me.GroupBox2.Controls.Add(Me.RadioButton1)
+        Me.GroupBox2.Location = New System.Drawing.Point(-1, 2)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(244, 301)
+        Me.GroupBox2.TabIndex = 13
+        Me.GroupBox2.TabStop = False
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Location = New System.Drawing.Point(81, 100)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(67, 17)
+        Me.RadioButton1.TabIndex = 14
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.Text = "Best of 3"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.Location = New System.Drawing.Point(81, 123)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(67, 17)
+        Me.RadioButton2.TabIndex = 15
+        Me.RadioButton2.TabStop = True
+        Me.RadioButton2.Text = "Best of 5"
+        Me.RadioButton2.UseVisualStyleBackColor = True
+        '
+        'RadioButton3
+        '
+        Me.RadioButton3.AutoSize = True
+        Me.RadioButton3.Location = New System.Drawing.Point(81, 146)
+        Me.RadioButton3.Name = "RadioButton3"
+        Me.RadioButton3.Size = New System.Drawing.Size(67, 17)
+        Me.RadioButton3.TabIndex = 16
+        Me.RadioButton3.TabStop = True
+        Me.RadioButton3.Text = "Best of 7"
+        Me.RadioButton3.UseVisualStyleBackColor = True
+        '
+        'RadioButton4
+        '
+        Me.RadioButton4.AutoSize = True
+        Me.RadioButton4.Location = New System.Drawing.Point(81, 169)
+        Me.RadioButton4.Name = "RadioButton4"
+        Me.RadioButton4.Size = New System.Drawing.Size(67, 17)
+        Me.RadioButton4.TabIndex = 17
+        Me.RadioButton4.TabStop = True
+        Me.RadioButton4.Text = "Best of 9"
+        Me.RadioButton4.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(81, 203)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 18
+        Me.Button2.Text = "Start Game"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(16, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(17, 13)
+        Me.Label3.TabIndex = 14
+        Me.Label3.Text = "X-"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(162, 0)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(18, 13)
+        Me.Label4.TabIndex = 15
+        Me.Label4.Text = "O-"
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(81, 0)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 14
+        Me.Button3.Text = "Reset Score"
+        Me.Button3.UseVisualStyleBackColor = True
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(236, 298)
-        Me.Controls.Add(Me.Label2)
-        Me.Controls.Add(Me.Label1)
-        Me.Controls.Add(Me.Button1)
-        Me.Controls.Add(Me.B7)
-        Me.Controls.Add(Me.B8)
-        Me.Controls.Add(Me.B9)
-        Me.Controls.Add(Me.B4)
-        Me.Controls.Add(Me.B5)
-        Me.Controls.Add(Me.B6)
-        Me.Controls.Add(Me.B3)
-        Me.Controls.Add(Me.B2)
-        Me.Controls.Add(Me.B1)
+        Me.ClientSize = New System.Drawing.Size(246, 305)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
         Me.Name = "Form1"
         Me.Text = " "
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
         Me.ResumeLayout(False)
-        Me.PerformLayout()
 
     End Sub
 
@@ -210,4 +332,14 @@ Partial Class Form1
     Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
     Friend WithEvents Label1 As Label
     Friend WithEvents Label2 As Label
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents RadioButton4 As RadioButton
+    Friend WithEvents RadioButton3 As RadioButton
+    Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents Button3 As Button
 End Class
--- tictaktoe/tictaktoe/Form1.vb
+++ tictaktoe/tictaktoe/Form1.vb
@@ -3,6 +3,7 @@
     Dim xs As Integer = 0
     Dim os As Integer = 0
     Dim stp As Integer = 0
+    Dim bestof As Integer
     Private Sub B1_Click(sender As Object, e As EventArgs) Handles B1.Click, B2.Click, B3.Click, B4.Click, B5.Click, B6.Click, B7.Click, B8.Click, B9.Click
         If Me.BackColor = DefaultBackColor Then
             If (sender.Text = "" And go = 0 Or sender.Text = "O") Then
@@ -16,28 +17,35 @@
         If winner() Then
             If stp = 0 Then
                 stp = 1
-                If go = 1 Then
-                    xs = xs + 1
-                    Label1.Text = xs
-                End If
                 If go = 0 Then
                     os = os + 1
                     Label2.Text = os
+                    If os = bestof Then
+                        MsgBox("O Is Winner")
+                        reset()
+                        XO()
+                        GroupBox1.Visible = False
+                        GroupBox2.Visible = True
+                    End If
                 End If
-                Me.BackColor = Color.Green
-                B1.BackColor = Color.Green
-                B2.BackColor = Color.Green
-                B3.BackColor = Color.Green
-                B4.BackColor = Color.Green
-                B5.BackColor = Color.Green
-                B6.BackColor = Color.Green
-                B7.BackColor = Color.Green
-                B8.BackColor = Color.Green
-                B9.BackColor = Color.Green
+                If go = 1 Then
+                    xs = xs + 1
+                    Label1.Text = xs
+                    If xs = bestof Then
+                        MsgBox("X Is Winner")
+                        reset()
+                        XO()
+                        GroupBox1.Visible = False
+                        GroupBox2.Visible = True
+                    End If
+                End If
+                If GroupBox1.Visible = True Then
+                    green()
+                End If
             End If
         End If
     End Sub
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+    Function reset() As Integer
         go = 0
         stp = 0
         B1.Text = ""
@@ -59,7 +67,25 @@
         B7.BackColor = DefaultBackColor
         B8.BackColor = DefaultBackColor
         B9.BackColor = DefaultBackColor
-    End Sub
+    End Function
+    Function XO() As Integer
+        xs = 0
+        Label1.Text = xs
+        os = 0
+        Label2.Text = os
+    End Function
+    Function green() As Integer
+        Me.BackColor = Color.Green
+        B1.BackColor = Color.Green
+        B2.BackColor = Color.Green
+        B3.BackColor = Color.Green
+        B4.BackColor = Color.Green
+        B5.BackColor = Color.Green
+        B6.BackColor = Color.Green
+        B7.BackColor = Color.Green
+        B8.BackColor = Color.Green
+        B9.BackColor = Color.Green
+    End Function
     Function winner() As Boolean
         If B1.Text = B2.Text And B2.Text = B3.Text And B1.Text <> "" Then Return True
         If B4.Text = B5.Text And B5.Text = B6.Text And B4.Text <> "" Then Return True
@@ -71,4 +97,28 @@
         If B3.Text = B5.Text And B5.Text = B7.Text And B3.Text <> "" Then Return True
         Return False
     End Function
+    Private Sub RadioButton1_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton1.CheckedChanged
+        bestof = 2
+    End Sub
+    Private Sub RadioButton2_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton2.CheckedChanged
+        bestof = 3
+    End Sub
+    Private Sub RadioButton3_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton3.CheckedChanged
+        bestof = 4
+    End Sub
+    Private Sub RadioButton4_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton4.CheckedChanged
+        bestof = 5
+    End Sub
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        reset()
+    End Sub
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        GroupBox1.Visible = True
+        GroupBox2.Visible = False
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        reset()
+        XO()
+    End Sub
 End Class
