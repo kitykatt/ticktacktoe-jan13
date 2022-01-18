5eca40d085c49258fd35a63c82d352f0ac71a347
kitykatt <90800597+kitykatt@users.noreply.github.com>
2022-01-18 14:08:42 -0700

    Add files via upload

---
+++ ticktacktoe-jan18/tictaktoe/tictaktoe/Form1.Designer.vb
@@ -0,0 +1,345 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.B1 = New System.Windows.Forms.Button()
+        Me.B2 = New System.Windows.Forms.Button()
+        Me.B3 = New System.Windows.Forms.Button()
+        Me.B4 = New System.Windows.Forms.Button()
+        Me.B5 = New System.Windows.Forms.Button()
+        Me.B6 = New System.Windows.Forms.Button()
+        Me.B7 = New System.Windows.Forms.Button()
+        Me.B8 = New System.Windows.Forms.Button()
+        Me.B9 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.BackgroundWorker1 = New System.ComponentModel.BackgroundWorker()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton3 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton4 = New System.Windows.Forms.RadioButton()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'B1
+        '
+        Me.B1.BackColor = System.Drawing.SystemColors.Control
+        Me.B1.Font = New System.Drawing.Font("Microsoft Sans Serif", 48.0!)
+        Me.B1.Location = New System.Drawing.Point(3, 19)
+        Me.B1.Margin = New System.Windows.Forms.Padding(0)
+        Me.B1.Name = "B1"
+        Me.B1.Padding = New System.Windows.Forms.Padding(3)
+        Me.B1.Size = New System.Drawing.Size(75, 75)
+        Me.B1.TabIndex = 0
+        Me.B1.UseVisualStyleBackColor = False
+        '
+        'B2
+        '
+        Me.B2.BackColor = System.Drawing.SystemColors.Control
+        Me.B2.Font = New System.Drawing.Font("Microsoft Sans Serif", 48.0!)
+        Me.B2.Location = New System.Drawing.Point(84, 19)
+        Me.B2.Margin = New System.Windows.Forms.Padding(0)
+        Me.B2.Name = "B2"
+        Me.B2.Padding = New System.Windows.Forms.Padding(3)
+        Me.B2.Size = New System.Drawing.Size(75, 75)
+        Me.B2.TabIndex = 1
+        Me.B2.UseVisualStyleBackColor = False
+        '
+        'B3
+        '
+        Me.B3.BackColor = System.Drawing.SystemColors.Control
+        Me.B3.Font = New System.Drawing.Font("Microsoft Sans Serif", 48.0!)
+        Me.B3.Location = New System.Drawing.Point(165, 19)
+        Me.B3.Margin = New System.Windows.Forms.Padding(0)
+        Me.B3.Name = "B3"
+        Me.B3.Padding = New System.Windows.Forms.Padding(3)
+        Me.B3.Size = New System.Drawing.Size(75, 75)
+        Me.B3.TabIndex = 2
+        Me.B3.UseVisualStyleBackColor = False
+        '
+        'B4
+        '
+        Me.B4.BackColor = System.Drawing.SystemColors.Control
+        Me.B4.Font = New System.Drawing.Font("Microsoft Sans Serif", 48.0!)
+        Me.B4.Location = New System.Drawing.Point(3, 100)
+        Me.B4.Margin = New System.Windows.Forms.Padding(0)
+        Me.B4.Name = "B4"
+        Me.B4.Padding = New System.Windows.Forms.Padding(3)
+        Me.B4.Size = New System.Drawing.Size(75, 75)
+        Me.B4.TabIndex = 5
+        Me.B4.UseVisualStyleBackColor = False
+        '
+        'B5
+        '
+        Me.B5.BackColor = System.Drawing.SystemColors.Control
+        Me.B5.Font = New System.Drawing.Font("Microsoft Sans Serif", 48.0!)
+        Me.B5.Location = New System.Drawing.Point(84, 100)
+        Me.B5.Margin = New System.Windows.Forms.Padding(0)
+        Me.B5.Name = "B5"
+        Me.B5.Padding = New System.Windows.Forms.Padding(3)
+        Me.B5.Size = New System.Drawing.Size(75, 75)
+        Me.B5.TabIndex = 4
+        Me.B5.UseVisualStyleBackColor = False
+        '
+        'B6
+        '
+        Me.B6.BackColor = System.Drawing.SystemColors.Control
+        Me.B6.Font = New System.Drawing.Font("Microsoft Sans Serif", 48.0!)
+        Me.B6.Location = New System.Drawing.Point(165, 100)
+        Me.B6.Margin = New System.Windows.Forms.Padding(0)
+        Me.B6.Name = "B6"
+        Me.B6.Padding = New System.Windows.Forms.Padding(3)
+        Me.B6.Size = New System.Drawing.Size(75, 75)
+        Me.B6.TabIndex = 3
+        Me.B6.UseVisualStyleBackColor = False
+        '
+        'B7
+        '
+        Me.B7.BackColor = System.Drawing.SystemColors.Control
+        Me.B7.Font = New System.Drawing.Font("Microsoft Sans Serif", 48.0!)
+        Me.B7.Location = New System.Drawing.Point(3, 181)
+        Me.B7.Margin = New System.Windows.Forms.Padding(0)
+        Me.B7.Name = "B7"
+        Me.B7.Padding = New System.Windows.Forms.Padding(3)
+        Me.B7.Size = New System.Drawing.Size(75, 75)
+        Me.B7.TabIndex = 8
+        Me.B7.UseVisualStyleBackColor = False
+        '
+        'B8
+        '
+        Me.B8.BackColor = System.Drawing.SystemColors.Control
+        Me.B8.Font = New System.Drawing.Font("Microsoft Sans Serif", 48.0!)
+        Me.B8.Location = New System.Drawing.Point(84, 181)
+        Me.B8.Margin = New System.Windows.Forms.Padding(0)
+        Me.B8.Name = "B8"
+        Me.B8.Padding = New System.Windows.Forms.Padding(3)
+        Me.B8.Size = New System.Drawing.Size(75, 75)
+        Me.B8.TabIndex = 7
+        Me.B8.UseVisualStyleBackColor = False
+        '
+        'B9
+        '
+        Me.B9.BackColor = System.Drawing.SystemColors.Control
+        Me.B9.Font = New System.Drawing.Font("Microsoft Sans Serif", 48.0!)
+        Me.B9.Location = New System.Drawing.Point(165, 181)
+        Me.B9.Margin = New System.Windows.Forms.Padding(0)
+        Me.B9.Name = "B9"
+        Me.B9.Padding = New System.Windows.Forms.Padding(3)
+        Me.B9.Size = New System.Drawing.Size(75, 75)
+        Me.B9.TabIndex = 6
+        Me.B9.UseVisualStyleBackColor = False
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.Button1.Location = New System.Drawing.Point(3, 262)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(237, 34)
+        Me.Button1.TabIndex = 9
+        Me.Button1.Text = "Clear"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(39, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(13, 13)
+        Me.Label1.TabIndex = 10
+        Me.Label1.Text = "0"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(185, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(13, 13)
+        Me.Label2.TabIndex = 11
+        Me.Label2.Text = "0"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Button3)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.B1)
+        Me.GroupBox1.Controls.Add(Me.B2)
+        Me.GroupBox1.Controls.Add(Me.Button1)
+        Me.GroupBox1.Controls.Add(Me.B3)
+        Me.GroupBox1.Controls.Add(Me.B7)
+        Me.GroupBox1.Controls.Add(Me.B6)
+        Me.GroupBox1.Controls.Add(Me.B8)
+        Me.GroupBox1.Controls.Add(Me.B5)
+        Me.GroupBox1.Controls.Add(Me.B9)
+        Me.GroupBox1.Controls.Add(Me.B4)
+        Me.GroupBox1.Location = New System.Drawing.Point(3, 2)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(244, 301)
+        Me.GroupBox1.TabIndex = 12
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Visible = False
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.Button2)
+        Me.GroupBox2.Controls.Add(Me.RadioButton4)
+        Me.GroupBox2.Controls.Add(Me.RadioButton3)
+        Me.GroupBox2.Controls.Add(Me.RadioButton2)
+        Me.GroupBox2.Controls.Add(Me.RadioButton1)
+        Me.GroupBox2.Location = New System.Drawing.Point(-1, 2)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(244, 301)
+        Me.GroupBox2.TabIndex = 13
+        Me.GroupBox2.TabStop = False
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Location = New System.Drawing.Point(81, 100)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(67, 17)
+        Me.RadioButton1.TabIndex = 14
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.Text = "Best of 3"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.Location = New System.Drawing.Point(81, 123)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(67, 17)
+        Me.RadioButton2.TabIndex = 15
+        Me.RadioButton2.TabStop = True
+        Me.RadioButton2.Text = "Best of 5"
+        Me.RadioButton2.UseVisualStyleBackColor = True
+        '
+        'RadioButton3
+        '
+        Me.RadioButton3.AutoSize = True
+        Me.RadioButton3.Location = New System.Drawing.Point(81, 146)
+        Me.RadioButton3.Name = "RadioButton3"
+        Me.RadioButton3.Size = New System.Drawing.Size(67, 17)
+        Me.RadioButton3.TabIndex = 16
+        Me.RadioButton3.TabStop = True
+        Me.RadioButton3.Text = "Best of 7"
+        Me.RadioButton3.UseVisualStyleBackColor = True
+        '
+        'RadioButton4
+        '
+        Me.RadioButton4.AutoSize = True
+        Me.RadioButton4.Location = New System.Drawing.Point(81, 169)
+        Me.RadioButton4.Name = "RadioButton4"
+        Me.RadioButton4.Size = New System.Drawing.Size(67, 17)
+        Me.RadioButton4.TabIndex = 17
+        Me.RadioButton4.TabStop = True
+        Me.RadioButton4.Text = "Best of 9"
+        Me.RadioButton4.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(81, 203)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 18
+        Me.Button2.Text = "Start Game"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(16, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(17, 13)
+        Me.Label3.TabIndex = 14
+        Me.Label3.Text = "X-"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(162, 0)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(18, 13)
+        Me.Label4.TabIndex = 15
+        Me.Label4.Text = "O-"
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(81, 0)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 14
+        Me.Button3.Text = "Reset Score"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(246, 305)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = " "
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents B1 As Button
+    Friend WithEvents B2 As Button
+    Friend WithEvents B3 As Button
+    Friend WithEvents B4 As Button
+    Friend WithEvents B5 As Button
+    Friend WithEvents B6 As Button
+    Friend WithEvents B7 As Button
+    Friend WithEvents B8 As Button
+    Friend WithEvents B9 As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents RadioButton4 As RadioButton
+    Friend WithEvents RadioButton3 As RadioButton
+    Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents Button3 As Button
+End Class
